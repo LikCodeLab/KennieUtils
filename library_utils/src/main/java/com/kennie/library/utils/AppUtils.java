2684183ab81ec420f08f2b0fce22d0b2da1bf9f7
kennie <245794335@qq.com>
2021-11-16 17:30:54 +0800

    update AppUtils

--- library_utils/src/main/java/com/kennie/library/utils/AppUtils.java
+++ library_utils/src/main/java/com/kennie/library/utils/AppUtils.java
@@ -1,6 +1,7 @@
 package com.kennie.library.utils;
 
 import android.content.Context;
+import android.content.pm.ApplicationInfo;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
 
@@ -15,6 +16,45 @@ import com.kennie.library.KennieUtilsInit;
  * @创建时间 2021/11/5 20:54
  */
 public class AppUtils {
+
+
+    /**
+     * 获取APP名称
+     *
+     * @return APP名称
+     */
+    public static String getAppName() {
+        return getAppName(KennieUtilsInit.getsApp());
+    }
+
+    /**
+     * 获取APP名称
+     *
+     * @param context 上下文
+     * @return APP名称
+     */
+    private static String getAppName(Context context) {
+        return getAppName(context, context.getPackageName());
+    }
+
+    /**
+     * 获取APP名称
+     *
+     * @param context     上下文
+     * @param packageName 包名
+     * @return APP名称
+     */
+    private static String getAppName(Context context, String packageName) {
+        PackageManager pm = context.getPackageManager();
+        String appName = null;
+        try {
+            ApplicationInfo applicationInfo = pm.getApplicationInfo(packageName, 0);
+            appName = String.valueOf(pm.getApplicationLabel(applicationInfo));
+        } catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+        }
+        return appName;
+    }
 
     /**
      * 获取APP版本名
