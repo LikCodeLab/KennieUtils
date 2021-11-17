548c1f7696d348c2d3dea8b7b9b4129c49f750ba
kennie <xdpvxv@163.com>
2021-11-18 01:18:44 +0800

    新增MMKV

--- app_example/src/main/java/com/kennie/example/utils/MyApp.java
+++ app_example/src/main/java/com/kennie/example/utils/MyApp.java
@@ -1,10 +1,9 @@
 package com.kennie.example.utils;
 
 import android.app.Application;
-import android.content.Context;
 
-import com.kennie.library.utils.IApp;
 import com.kennie.library.utils.KennieUtilsApp;
+import com.kennie.library.utils.config.MMKVConfig;
 
 /**
  * @项目名 KennieUtils
@@ -14,16 +13,13 @@ import com.kennie.library.utils.KennieUtilsApp;
  * @修改人
  * @创建时间 2021/11/5 21:41
  */
-public class MyApp extends Application implements IApp {
+public class MyApp extends Application {
 
     @Override
     public void onCreate() {
         super.onCreate();
         KennieUtilsApp.init(this);
+        MMKVConfig.init(this);
     }
 
-    @Override
-    public Context getAppContext() {
-        return this;
-    }
 }
