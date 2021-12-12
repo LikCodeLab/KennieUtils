3634314908ddb30a24f4b8b0c1639d4ae0cdc845
kennie <xdpvxv@163.com>
2021-12-12 21:57:01 +0800

    update AppUtils

--- app_example/src/main/java/com/kennie/example/utils/ExampleActivity.java
+++ app_example/src/main/java/com/kennie/example/utils/ExampleActivity.java
@@ -1,11 +1,18 @@
 package com.kennie.example.utils;
 
 import android.os.Bundle;
+import android.os.Environment;
 import android.util.Log;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.kennie.library.utils.AppUtilsCompat;
+import com.kennie.library.utils.old.core.AlgorithmEncryptUtil;
+import com.kennie.library.utils.old.core.AppMarketUtil;
+import com.kennie.library.utils.old.core.AppPathUtil;
 import com.kennie.library.utils.old.core.DateTimeUtil;
+import com.kennie.library.utils.old.core.PhoneUtil;
+import com.kennie.library.utils.old.core.RomUtil;
 
 import java.util.Date;
 
@@ -17,6 +24,47 @@ public class ExampleActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_example);
+
+        // AppUtilsCompat
+        Log.i(TAG, "APP名称：" + AppUtilsCompat.getAppName());
+        Log.i(TAG, "App包名：" + AppUtilsCompat.getAppPackage());
+        Log.i(TAG, "App版本名称 ：" + AppUtilsCompat.getAppVersionName());
+        Log.i(TAG, "App版本号 ：" + AppUtilsCompat.getAppVersionCode());
+        Log.i(TAG, "App图标 ：" + AppUtilsCompat.getAppIcon());
+        Log.i(TAG, "App安装原始路径：" + AppUtilsCompat.getAppInstallPath());
+        Log.i(TAG, "App原始安装文件(APK)：" + AppUtilsCompat.getAppSourceFile().getAbsolutePath());
+        Log.i(TAG, "APP是否安装：" + AppUtilsCompat.isAppInstalled());
+        Log.i(TAG, "APP是否安装：" + AppUtilsCompat.isAppInstalled("com.jskjjx.cloudmeeting"));
+        Log.i(TAG, "APP是否为Debug模式 ：" + AppUtilsCompat.isAppDebugMode());
+        Log.i(TAG, "App是否处于前台 ：" + AppUtilsCompat.isAppForeground());
+        // Log.i(TAG, "安装APP（兼容Android7.0及以上版本） ：");
+        // AppUtilsCompat.installApk(new File(""), "");
+
+
+        Log.i(TAG, "ROM：" + RomUtil.isXiaomi());
+
+
+        Log.i(TAG, "获取APP应用数据路径：" + AppPathUtil.getAppDataPath());
+        Log.i(TAG, "获取APP应用缓存路径：" + AppPathUtil.getAppCachePath());
+        Log.i(TAG, "获取APP应用数据库路径：" + AppPathUtil.getAppDbPath());
+        Log.i(TAG, "获取APP应用数据库路径：" + AppPathUtil.getAppDbPath("databaseName"));
+        Log.i(TAG, "获取APP应用文件路径：" + AppPathUtil.getAppFilesPath());
+        Log.i(TAG, "获取APP应用SP路径：" + AppPathUtil.getAppSpPath());
+
+        Log.i(TAG, "获取APP：" + AppPathUtil.getExternalStoragePath());
+        Log.i(TAG, "获取APP：" + AppPathUtil.getAppExternalDataPath());
+        Log.i(TAG, "获取APP：" + AppPathUtil.getAppExternalOBBPath());
+        Log.i(TAG, "获取APP：" + AppPathUtil.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES).getAbsolutePath());
+
+
+        Log.i(TAG, "获取APP：" + PhoneUtil.isPhone());
+
+
+        Log.i(TAG, "加密后的：" + AlgorithmEncryptUtil.getMD5("12345"));
+        Log.i(TAG, "加密后的：" + AlgorithmEncryptUtil.getHash("12345", AlgorithmEncryptUtil.SHA1));
+        Log.i(TAG, "加密后的：" + AlgorithmEncryptUtil.getHash("12345", AlgorithmEncryptUtil.SHA256));
+
+        Log.i(TAG, "获取APP市场：" + AppMarketUtil.isMarketAvailable());
 
         // DateUtils
         Log.i(TAG, "当前缺省格式日期与时间：" + DateTimeUtil.getDate());
@@ -32,39 +80,6 @@ public class ExampleActivity extends AppCompatActivity {
         Log.i(TAG, "获取新的间隔月数日期：" + DateTimeUtil.formatDate(DateTimeUtil.getDateByIntervalMonths(new Date(), 1), DateTimeUtil.DEFAULT_DATE_PATTERN_YMD));
         Log.i(TAG, "获取新的间隔天数日期：" + DateTimeUtil.formatDate(DateTimeUtil.getDateByIntervalDays(new Date(), 3), DateTimeUtil.DEFAULT_DATE_PATTERN_YMD));
 
-
-        //        Log.i(TAG, "APP名称：" + AppUtil.getAppName());
-//        Log.i(TAG, "APP版本名：" + AppUtil.getAppVersionName());
-//        Log.i(TAG, "APP安装路径：" + AppUtil.getAppInstallSourcePath());
-//        Log.i(TAG, "APP安装文件：" + AppUtil.getAppSourceFile().getName());
-//
-//        Log.i(TAG, "APP安装文件：" + AppUtil.isAppInstalled("com.jskjjx.cloudmeeting"));
-
-
-//        Log.i(TAG, "ROM：" + RomUtil.isXiaomi());
-
-//
-//        Log.i(TAG, "获取APP应用数据路径：" + AppPathUtil.getAppDataPath());
-//        Log.i(TAG, "获取APP应用缓存路径：" + AppPathUtil.getAppCachePath());
-//        Log.i(TAG, "获取APP应用数据库路径：" + AppPathUtil.getAppDbPath());
-//        Log.i(TAG, "获取APP应用数据库路径：" + AppPathUtil.getAppDbPath("databaseName"));
-//        Log.i(TAG, "获取APP应用文件路径：" + AppPathUtil.getAppFilesPath());
-//        Log.i(TAG, "获取APP应用SP路径：" + AppPathUtil.getAppSpPath());
-//
-//        Log.i(TAG, "获取APP：" + AppPathUtil.getExternalStoragePath());
-//        Log.i(TAG, "获取APP：" + AppPathUtil.getAppExternalDataPath());
-//        Log.i(TAG, "获取APP：" + AppPathUtil.getAppExternalOBBPath());
-//        Log.i(TAG, "获取APP：" + AppPathUtil.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES).getAbsolutePath());
-//
-//
-//        Log.i(TAG, "获取APP：" + PhoneUtil.isPhone());
-//
-//
-//        Log.i(TAG, "加密后的：" + AlgorithmEncryptUtil.getMD5("12345"));
-//        Log.i(TAG, "加密后的：" + AlgorithmEncryptUtil.getHash("12345", AlgorithmEncryptUtil.SHA1));
-//        Log.i(TAG, "加密后的：" + AlgorithmEncryptUtil.getHash("12345", AlgorithmEncryptUtil.SHA256));
-//
-//        Log.i(TAG, "获取APP市场：" + AppMarketUtil.isMarketAvailable());
 
     }
 }
--- library_utils/src/main/java/com/kennie/library/utils/AppUtilsCompat.java
+++ library_utils/src/main/java/com/kennie/library/utils/AppUtilsCompat.java
@@ -1,9 +1,20 @@
 package com.kennie.library.utils;
 
+import android.app.ActivityManager;
+import android.content.Context;
+import android.content.Intent;
 import android.content.pm.ApplicationInfo;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
 import android.graphics.drawable.Drawable;
+import android.net.Uri;
+import android.os.Build;
+import android.provider.Settings;
+
+import androidx.core.content.FileProvider;
+
+import java.io.File;
+import java.util.List;
 
 /**
  * @项目名 KennieUtils
@@ -14,11 +25,18 @@ import android.graphics.drawable.Drawable;
  * @创建时间 2021/12/11 10:48
  *
  * <p>
- * --获取App名称        {@link #getAppName()}
- * --获取App包名        {@link #getAppPackage()}
- * --获取App版本名称    {@link #getAppVersionName()}
- * --获取App版本号      {@link #getAppVersionCode()}
- * --获取App图标       {@link #getAppIcon()}
+ * --获取App名称                                {@link #getAppName()}
+ * --获取App包名                                {@link #getAppPackage()}
+ * --获取App版本名称                             {@link #getAppVersionName()}
+ * --获取App版本号                               {@link #getAppVersionCode()}
+ * --获取App图标                                {@link #getAppIcon()}
+ * --获取App安装原始路径                         {@link #getAppInstallPath()}
+ * --获取App原始安装文件(APK)                    {@link #getAppSourceFile()}
+ * --判断APP是否安装                            {@link #isAppInstalled()}
+ * --判断APP是否安装                            {@link #isAppInstalled(String packageName)}
+ * --判断APP是否为Debug模式                      {@link #isAppDebugMode()}
+ * --判断App是否处于前台                         {@link #isAppForeground()}
+ * --安装APP（兼容Android7.0及以上版本）          {@link #installApk(File file, String authority)}
  * </p>
  */
 
@@ -56,7 +74,7 @@ public class AppUtilsCompat {
      *
      * @return the application's app version name
      */
-    private static String getAppVersionName() {
+    public static String getAppVersionName() {
         try {
             PackageManager packageManager = KennieUtilInit.getAppContext().getPackageManager();
             PackageInfo packageInfo = packageManager.getPackageInfo(KennieUtilInit.getAppContext().getPackageName(), 0);
@@ -73,7 +91,7 @@ public class AppUtilsCompat {
      *
      * @return the application's app version code
      */
-    private static int getAppVersionCode() {
+    public static int getAppVersionCode() {
         try {
             PackageManager packageManager = KennieUtilInit.getAppContext().getPackageManager();
             PackageInfo packageInfo = packageManager.getPackageInfo(KennieUtilInit.getAppContext().getPackageName(), 0);
@@ -90,7 +108,7 @@ public class AppUtilsCompat {
      *
      * @return the application's app icon
      */
-    private static Drawable getAppIcon() {
+    public static Drawable getAppIcon() {
         PackageManager pm = KennieUtilInit.getAppContext().getPackageManager();
         try {
             ApplicationInfo applicationInfo = pm.getApplicationInfo(KennieUtilInit.getAppContext().getPackageName(), 0);
@@ -101,4 +119,143 @@ public class AppUtilsCompat {
         }
     }
 
+
+    /**
+     * APP安装原始路径
+     *
+     * @return the application's app path
+     */
+    public static String getAppInstallPath() {
+        try {
+            ApplicationInfo applicationInfo = KennieUtilInit.getAppContext().getPackageManager().getApplicationInfo(KennieUtilInit.getAppContext().getPackageName(), 0);
+            return applicationInfo.sourceDir;
+        } catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+            return "";
+        }
+    }
+
+    /**
+     * APP原始安装文件(APK)
+     *
+     * @return the application's app file.apk
+     */
+    public static File getAppSourceFile() {
+        String sourceDir = getAppInstallPath();
+        if (sourceDir != null) {
+            return new File(sourceDir);
+        }
+        return null;
+    }
+
+
+    /**
+     * 判断APP应用是否安装
+     *
+     * @return true|false
+     */
+    public static boolean isAppInstalled() {
+        return isAppInstalled(KennieUtilInit.getAppContext().getPackageName());
+    }
+
+
+    /**
+     *
+     *
+     * @return
+     */
+    /**
+     * 判断APP应用是否安装
+     *
+     * @param packageName 包名
+     * @return true|false
+     */
+    public static boolean isAppInstalled(String packageName) {
+        PackageManager pm = KennieUtilInit.getAppContext().getPackageManager();
+        try {
+            PackageInfo info = pm.getPackageInfo(packageName, 0);
+            return info != null;
+        } catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+
+    /**
+     * 判断APP是否为Debug模式
+     *
+     * @return true|false
+     */
+    public static boolean isAppDebugMode() {
+        try {
+            String packageName = KennieUtilInit.getAppContext().getPackageName();
+            PackageInfo packageInfo = KennieUtilInit.getAppContext().getPackageManager().getPackageInfo(packageName, PackageManager.GET_ACTIVITIES);
+            if (packageInfo != null) {
+                ApplicationInfo info = packageInfo.applicationInfo;
+                return (info.flags & ApplicationInfo.FLAG_DEBUGGABLE) != 0;
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return false;
+    }
+
+    /**
+     * 判断App是否处于前台
+     *
+     * @return true|false
+     */
+    public static boolean isAppForeground() {
+        ActivityManager am = (ActivityManager) KennieUtilInit.getApp().getSystemService(Context.ACTIVITY_SERVICE);
+        if (am == null) return false;
+        List<ActivityManager.RunningAppProcessInfo> info = am.getRunningAppProcesses();
+        if (info == null || info.size() == 0) return false;
+        for (ActivityManager.RunningAppProcessInfo aInfo : info) {
+            if (aInfo.importance == ActivityManager.RunningAppProcessInfo.IMPORTANCE_FOREGROUND) {
+                if (aInfo.processName.equals(KennieUtilInit.getApp().getPackageName())) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+
+    /**
+     * 安装APP（兼容Android7.0及以上版本）
+     *
+     * @param file      App文件路径
+     * @param authority The authority of a FileProvider defined in a <provider> element in your app's manifest.
+     */
+    public static void installApk(File file, String authority) {
+        if (file == null || !file.exists()) {
+            return;
+        }
+        try {
+            PackageManager pm = KennieUtilInit.getApp().getPackageManager();
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                if (!pm.canRequestPackageInstalls()) {
+                    Intent intent = new Intent(Settings.ACTION_MANAGE_UNKNOWN_APP_SOURCES, Uri.parse("package:" + KennieUtilInit.getApp().getPackageName()));
+                    intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                    KennieUtilInit.getApp().startActivity(intent);
+                    return;
+                }
+            }
+            Intent intent = new Intent(Intent.ACTION_VIEW);
+            Uri uri;
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+                intent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
+                uri = FileProvider.getUriForFile(KennieUtilInit.getApp(), authority, file);
+            } else {
+                uri = Uri.fromFile(file);
+            }
+            intent.setDataAndType(uri, "application/vnd.android.package-archive");
+            intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            KennieUtilInit.getApp().startActivity(intent);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
